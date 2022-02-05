e46d0a79d5e3e2b829e9bfcd0a0b910ef5293927
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2022-02-05 22:28:20 +0100

    wip: pwa test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import EmailEditor from "react-email-editor";
+import "./App.css";
 
 function App() {
+  const emailEditorRef = useRef<any>(null);
+
+  const onLoad = () => {
+    // editor instance is created
+    // you can load your template here;
+    // const templateJson = {};
+    // emailEditorRef.current.editor.loadDesign(templateJson);
+  };
+
+  const onReady = () => {
+    // editor is ready
+    console.log("onReady");
+  };
+
+  const exportHtml = () => {
+    emailEditorRef.current.editor.exportHtml((data: any) => {
+      const { design, html } = data;
+      console.log("exportHtml", html);
+      console.log("exportHtml", design);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="nav">
+        <button onClick={() => exportHtml()}>Export HTML</button>
+      </div>
+      <EmailEditor
+        onLoad={onLoad}
+        ref={emailEditorRef}
+        tools={{
+          social: {
+            enabled: true,
+          },
+        }}
+        options={{
+          features: {
+            imageEditor: true,
+          },
+          user: {
+            id: 121212,
+            name: "Tioluwani Kolawole",
+            email: "tioluwani@simpu.co",
+          },
+          mergeTags: [
+            {
+              name: "Profile",
+              mergeTags: [{ name: "First name", value: "{{first_name}}" }],
+            },
+          ],
+        }}
+        appearance={{
+          theme: "dark",
+          panels: {
+            tools: {
+              dock: "left",
+            },
+          },
+        }}
+        projectId={62492}
+        style={{ height: "100vh" }}
+      />
     </div>
   );
 }
